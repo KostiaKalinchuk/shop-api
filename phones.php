98dd542119ef9804100223df66d4c6d3e4825b15
Your Name <9leon1@gmail.com>
2018-06-17 21:31:48 +0300

    add phones

--- phones.php
+++ phones.php
@@ -14,13 +14,32 @@ or die("Ошибка " . mysqli_error($link));
 
 
 // выполняем операции с базой данных
-$sql = "SELECT * FROM Categories";
+$sql = "SELECT * FROM Phones";
 $result = mysqli_query($link, $sql) or die("Ошибка " . mysqli_error($link));
 
 while ($row = mysqli_fetch_array($result)) {
     $rows[] = array(
         "id" => $row['id'],
-        "name" => $row['name']);
+        "categoryId" => $row['categoryId'],
+        "name" => $row['name'],
+        "description" => $row['description'],
+        "price" => $row['price'],
+//        "image" => $row['image'],
+        "cpu" => $row['cpu'],
+        "camera" => $row['camera'],
+        "size" => $row['size'],
+        "weight" => $row['weight'],
+        "display" => $row['display'],
+        "battery" => $row['battery'],
+        "memory" => $row['memory'],
+        "color" => $row['color'],
+        "system" => $row['system'],
+        "connection" => $row['connection'],
+        "material" => $row['material'],
+        "navigation " => $row['navigation'],
+        "audio" => $row['audio'],
+        "video processor" => $row['video processor']
+    );
 }
 
 $json = json_encode($rows);
