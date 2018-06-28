9bf7e026fd7413030c54aa34eb330c6822eda7d5
Your Name <9leon1@gmail.com>
2018-06-28 15:33:48 +0300

    Removed folder from repository

--- templates/admin/include/header.php
+++
@@ -1,5 +0,0 @@
-      <div id="adminHeader">
-        <h2>Адмінпанель</h2>
-        <p>Ви увійшли як <b><?php echo htmlspecialchars( $_SESSION['username']) ?></b>. <a href="admin.php?action=listArticles">Редагувати артикули</a> | <a href="admin.php?action=listCategories">Редагувати категорії</a> | <a href="admin.php?action=logout"?>Вийти</a></p>
-      </div>
-
